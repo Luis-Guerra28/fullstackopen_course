088bab57e1bdaead3776e55dc253905124fa085a
Luis Guerra <luis910gg@gmail.com>
2024-04-03 19:14:06 -0400

    Exercise 1.7 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,12 +6,23 @@ const Button = ({handleClick, text}) => {
   )
 }
 
-const Display = ({ good, neutral, bad}) => {
+const Display = ({ good, neutral, bad, total}) => {
+  
+  const good_points = 1 * good
+  const bad_points = -1 * bad
+
+  const average = (good_points + bad_points )/total
+  const positive_porcentage = (good/total) * 100
+
+
   return (
     <>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {average}</div>
+      <div>positive {positive_porcentage}%</div>
     </>
     )
 }
@@ -20,17 +31,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
   
   const handleClickGood = () => {
     setGood(good+1)
+    setTotal(total + 1)
   }
 
   const handleClickNeutral = () => {
     setNeutral(neutral+1)
+    setTotal(total + 1)
   }
 
   const handleClickBad = () => {
     setBad(bad+1)
+    setTotal(total + 1)
   }
 
   return (
@@ -40,7 +56,7 @@ const App = () => {
       <Button handleClick={handleClickNeutral} text={'neutral'}/>
       <Button handleClick={handleClickBad} text={'bad'}/>
       <h1>statistic</h1>
-      <Display good={good} neutral={neutral} bad={bad}/>
+      <Display good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   )
 }
